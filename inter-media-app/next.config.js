1dfcad5cf48c3f5f0651b4f8823e1f89dd413416
saptoprawiroutomo <84174626+saptoprawiroutomo@users.noreply.github.com>
2026-01-25 14:56:20 +0000

    Simplify next.config for Vercel deployment

--- inter-media-app/next.config.js
+++ inter-media-app/next.config.js
@@ -1,47 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'epson.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ssl-product-images.www8-hp.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.canon.co.id',
-      },
-      {
-        protocol: 'https',
-        hostname: 'dummyimage.com',
-      },
-    ],
     unoptimized: true
   },
-  trailingSlash: false,
-  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
